6292aefbb8311531515779a1ecefe300b168afb1
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-03-16 05:49:54 +0100

    Added left rotation

---
+++ hackerrank/ctci-array-left-rotation.cs
@@ -0,0 +1,70 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+    public static List<int> rotLeft(List<int> a, int d)
+    {
+        var result = new List<int>(Enumerable.Range(0, a.Count));
+
+        d %= a.Count;
+
+        for (var i = 0; i < a.Count; ++i)
+        {
+            var newIndex = (i - d);
+            if (newIndex < 0)
+            {
+                newIndex += a.Count;
+            }
+
+            result[newIndex] = a[i];
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> result = Result.rotLeft(a, d);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
