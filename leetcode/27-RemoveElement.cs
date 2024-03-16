316f6e6b4a7dc3543326089f2846713dce82d02e
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-03-16 07:23:53 +0100

    format 27

--- leetcode/27-RemoveElement.cs
+++ leetcode/27-RemoveElement.cs
@@ -1,16 +1,15 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
         var leftItems = nums.Length;
-
         var right = nums.Length - 1;
 
-        for(var left = 0; left <= right && left < nums.Length; ++left)
+        for (var left = 0; left <= right && left < nums.Length; ++left)
         {
-            while(nums[left] == val)
+            while (nums[left] == val)
             {
                 leftItems--;
 
-                if(left >= right)
+                if (left >= right)
                 {
                     break;
                 }
