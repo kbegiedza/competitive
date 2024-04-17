c822b0bda8730d35ed64fe26cd49b919a7f99cbd
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-04-17 07:23:10 +0200

    27 ++

--- leetcode/27-RemoveElement.cs
+++ leetcode/27-RemoveElement.cs
@@ -22,3 +22,40 @@ public class Solution {
         return leftItems;
     }
 }
+
+/*
+public class Solution {
+    public int RemoveElement(int[] nums, int val)
+    {
+        var swapsCounter = 0;
+
+        var rightIter = nums.Length - 1;
+
+        // find first occurrence from end
+        while(rightIter >= 0 && nums[rightIter] == val)
+        {
+            --rightIter;
+            ++swapsCounter;
+        }
+
+        for (var leftIter = 0; leftIter <= rightIter; ++leftIter)
+        {
+            if(nums[leftIter] == val)
+            {
+                // swap
+                nums[leftIter] = nums[rightIter];
+                nums[rightIter] = val;
+
+                // find next from end
+                while(rightIter >= 0 && nums[rightIter] == val)
+                {
+                    --rightIter;
+                    ++swapsCounter;
+                }
+            }
+        }
+
+        return nums.Length - swapsCounter;
+    }
+}
+*/
