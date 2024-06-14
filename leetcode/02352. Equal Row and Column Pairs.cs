36d025c629bcf08f988aa7272a362be48bf8b85b
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-06-14 07:08:26 +0200

    optimized 2352

--- leetcode/02352. Equal Row and Column Pairs.cs
+++ leetcode/02352. Equal Row and Column Pairs.cs
@@ -1,27 +1,48 @@
 public class Solution {
     public int EqualPairs(int[][] grid) {
+        var rowBuilder = new StringBuilder();
+        var columnBuilder = new StringBuilder();
+        var occurrences = new Dictionary<string, int>();
+
+        for (var i = 0; i < grid.Length; ++i)
+        {
+            for (var j = 0; j < grid.Length; ++j)
+            {
+                rowBuilder.Append(grid[i][j]);
+                rowBuilder.Append('+');
+            }
+
+            var row = rowBuilder.ToString();
+            if (occurrences.TryGetValue(row, out var count))
+            {
+                occurrences[row] = count + 1;
+            }
+            else
+            {
+                occurrences[row] = 1;
+            }
+
+            rowBuilder.Clear();
+        }
+
         var result = 0;
 
         for (var i = 0; i < grid.Length; ++i)
         {
             for (var j = 0; j < grid.Length; ++j)
             {
-                var isOk = true;
-
-                for(var item = 0; item < grid.Length; ++item)
-                {
-                    if (grid[i][item] != grid [item][j])
-                    {
-                        isOk = false;
-                        break;
-                    }
-                }
-
-                if(isOk)
-                {
-                    result++;
-                }
+                columnBuilder.Append(grid[j][i]);
+                columnBuilder.Append('+');
             }
+
+            var column = columnBuilder.ToString();
+
+            if (occurrences.TryGetValue(column, out var count))
+            {
+                result += occurrences[column];
+            }
+
+            columnBuilder.Clear();
         }
 
         return result;
