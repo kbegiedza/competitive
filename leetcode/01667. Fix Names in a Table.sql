3f6cb74413e853d3c674f74d10496acb5150cc23
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-12-09 20:44:16 +0100

    Update 01667. Fix Names in a Table.sql

--- leetcode/01667. Fix Names in a Table.sql
+++ leetcode/01667. Fix Names in a Table.sql
@@ -1,3 +1,4 @@
 -- Write your PostgreSQL query statement below
-SELECT t_u.user_id, INITCAP(t_u.name) AS "name"
+SELECT t_u.user_id, UPPER(LEFT(t_u.name, 1)) || LOWER(SUBSTRING(t_u.name FROM 2)) AS "name"
 FROM Users AS t_u
+ORDER BY t_u.user_id
